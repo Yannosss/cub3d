99f47b27a88fa66a0617a12e1a81d1ee5ec67c71
Julia BATORO <jbatoro@e1r6p8.clusters.42paris.fr>
2022-09-20 15:52:44 +0200

    julia : parsing

--- srcs/20_parser/parser.c
+++ srcs/20_parser/parser.c
@@ -1,14 +1,7 @@
 #include "cub3D.h"
 
-/*Check if map is valid */
-int	ft_get_file_content(t_data *data, char *file)
-{
-	data->file_content = ft_read_file(data, file);
-	return (0);
-}
-
 /* Create the map */
-char	**ft_create_map(char *map, t_data *data)
+char	**ft_create_file(char *map, t_data *data)
 {
 	char	**final_map;
 
@@ -19,72 +12,6 @@ char	**ft_create_map(char *map, t_data *data)
 	return (final_map);
 }
 
-char	*ft_get_textures(t_data *data, char *str)
-{
-	int i;
-	int j;
-
-	j = 0;
-	i = 0;
-	char **tmp;
-	//data->textures->save = malloc(sizeof(char*) * (1000 * 2));
-	while (str[i])
-	{
-		if (str[i] == ' ')
-			i++;
-		if (str[i] == 'N' && str[i + 1] == 'O')
-		{
-			if(data->textures->no == 1)
-				exit(0);
-				//return(ft_error_check_map(data,"error: already found NO"));
-			printf("Found NO\n");
-			tmp = ft_split(str, ' ');
-			data->textures->save[j] = tmp[0];
-			//printf("ID = %s\n", tmp[0]);
-			data->textures->no = 1;
-			//ft_free_split(tmp);
-			j++;
-		}
-		if (str[i] == 'S' && str[i + 1] == 'O')
-		{
-			if(data->textures.so == 1)
-				return(ft_error_check_map(data,"ERROR, already found SO"));
-			printf("Found SO\n");
-			tmp = ft_split(str, ' ');
-			data->textures.save[j] = tmp[0];
-			printf("ID = %s\n", data->textures.save[j]);
-			data->textures.so = 1;
-			//ft_free_split(tmp);
-			j++;
-		}
-		if (str[i] == 'W' && str[i + 1] == 'E')
-		{
-			if(data->textures.we == 1)
-				return(ft_error_check_map(data,"ERROR, already found WE"));
-			printf("Found WE\n");
-			tmp = ft_split(str, ' ');
-			data->textures.save[j] = tmp[0];
-			//printf("ID = %s\n", data->textures.save[j]);
-			data->textures.we = 1;
-			//ft_free_split(tmp);
-			j++;
-		}
-		if (str[i] == 'E' && str[i + 1] == 'A')
-		{
-			if(data->textures.ea == 1)
-				return(ft_error_check_map(data,"ERROR, already found EA"));
-			printf("Found EA\n");
-			tmp = ft_split(str, ' ');
-			data->textures.save[j] = tmp[0];
-			printf("ID = %s\n", data->textures.save[j]);
-			data->textures.ea = 1;
-			//ft_free_split(tmp);
-			j++;
-		}
-		i++;
-	}
-	return(0);
-}
 /* Read map */
 char	**ft_read_file(t_data *data, char *file_input)
 {
@@ -92,8 +19,6 @@ char	**ft_read_file(t_data *data, char *file_input)
 	char	*file;
 	int		fd;
 
-	data->textures->save = malloc(sizeof(char *) * 1000); //calculer bonne taille de tableau
-	// garbage
 	str = "";
 	file = ft_strdup("");
 	fd = open(file_input, O_RDONLY);
@@ -104,9 +29,6 @@ char	**ft_read_file(t_data *data, char *file_input)
 		str = get_next_line(fd);
 		if (str == NULL || str[0] == '\n')
 			break ;
-		ft_get_textures(data, str);
-		printf(" ID = %s\n", data->textures->save[0]);
-		//ft_get_colors()
 		file = ft_strjoin(file, str);
 		//inclure garbage
 		free(str);
@@ -114,7 +36,7 @@ char	**ft_read_file(t_data *data, char *file_input)
 	}
 	free(str);
 	close(fd);
-	return (ft_create_map(file, data));
+	return (ft_create_file(file, data));
 }
 
 /*--------------------------- PARSER -------------------------------------
@@ -124,15 +46,20 @@ char	**ft_read_file(t_data *data, char *file_input)
 4- If map exist in the file, get it into a new char** > data->map
 	Else, return error.
 --------------------------------------------------------------------------*/
+
 int	ft_parser(t_data *data, char **av)
 {
-	ft_read_file(data, av[1]);
-	//ft_get_file_content(data, av[1]);
-	//if (ft_is_map_first(data))
-	//	return(ft_error_check_map(data, "error: file content is not in good order"));
-	//if (ft_check_dir_order(data, 'N', 'O'))
-	//	return(ft_error_check_map(data, "error: wrong directions positions"));
-	//data->map = ft_get_map(data);
-	//ft_check_map(data);
-	return(0);
+	int index_c;
+
+	index_c = 0; 
+	data->file_content = ft_read_file(data, av[1]);
+	//ft_print_map(data, data->file_content);
+	if (!data->file_content)
+		return(ft_error_check_map(data, "error: empty file"));
+	index_c = ft_parse_colums(data);
+	data->map_start = index_c; //get le debut de la map;
+	printf(COLOR_CYAN"DEBUT MAP : %d"COLOR_NORMAL, data->map_start);
+	// map_check 
+	ft_get_map(data);
+	return (0);
 }
--- srcs/main_julia.c
+++ srcs/main_julia.c
@@ -1,26 +1,35 @@
 #include "cub3D.h"
 
+void    ft_init_colors(t_data *data)
+{
+    data->colors.c = 0;
+    data->colors.f = 0;
+    data->colors.r = 0;
+    data->colors.g = 0;
+    data->colors.g = 0;
+    data->colors.alpha = 0;
+}
+
+void    ft_init_textures(t_data *data)
+{
+    data->textures.north = NULL;
+    data->textures.south = NULL;
+    data->textures.west = NULL;
+    data->textures.east = NULL;
+}
+
 int    ft_init_struct(t_data *data)
 {
     data = malloc(sizeof(t_data));
     if (!data)
         return (-1);
-   
-    //data->textures = NULL;
     data->map = NULL;
     data->map_height= 0;
     data->map_width = 0;
     data->nb_player = 0;
     data->file_content = NULL;
-    /*data->textures->north = NULL;
-    data->textures->south = NULL;
-    data->textures->west = NULL;
-    data->textures->east = NULL;
-    data->textures->no = 0;
-    data->textures->so = 0;
-    data->textures->we = 0;
-    data->textures->ea = 0;*/
-    
+    ft_init_textures(data);
+    ft_init_colors(data);
     return(0);
 }
 
@@ -31,8 +40,5 @@ int main(int ac, char **av)
     ft_check_input_and_format(ac, av);
     ft_init_struct(&data);
     ft_parser(&data, av);
-    printf("ID = %s\n", data.textures->save[0]);
-    // debug
-    //ft_print_map(&data, data.file_content);
     return(0);
 }
