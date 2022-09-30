e93fd310d79ba6b228ca2b701a0e7c1d9b8ba446
Julia BATORO <jbatoro@e1r3p21.clusters.42paris.fr>
2022-09-30 14:32:45 +0200

    ok

--- srcs/10_initialisation/data_initialisation.c
+++ srcs/10_initialisation/data_initialisation.c
@@ -1,5 +1,27 @@
 #include "cub3D.h"
 
+void    ft_init_img(t_data *data)
+{
+    data->img.img_pt = NULL;
+    data->img.addr = NULL;
+    data->img.bpp = -1;
+    data->img.line_len = -1;
+    data->img.endian = -1;
+    data->img.width = -1;
+    data->img.height = -1;
+}
+
+void    ft_init_player(t_data *data)
+{
+	data->player.x = -1.0;
+	data->player.y = -1.0;
+	data->player.direction = -1.0;
+    data->player.direction = -1;
+    data->player.cos_direction = -1;
+    data->player.sin_direction = -1;
+    data->player.tan_direction = -1;
+}
+
 void    ft_init_colors(t_data *data)
 {
     data->ceiling_clr.checked = 0;
@@ -24,58 +46,19 @@ void    ft_init_textures(t_data *data)
 
 void	ft_data_initialisation(t_data *data)
 {
-	// check a la fin que toutes les variable de data sont initisalisees
 	data->garbage_collector = NULL;
 	data->map = NULL;
+    data->file_content = NULL;
 	data->nb_player = 0;
-	data->player.x = -1.0;
-	data->player.y = -1.0;
-	data->player.direction = -1.0;
+    data->id_filled = -1;
 	data->map_width = 0;
 	data->map_height = 0;
-	data->nb_player = 0;
-    data->id_filled = 0;
-    data->file_content = NULL;
-    data->player.direction = 0;
 	data->d_camera = (((double)WINDOW_WIDTH - 1.0) / 2.0) / (tan (FOV / 2.0));
 	data->mlx = NULL;
 	data->window = NULL;
+    data->map_start = 0;
+    ft_init_img(data);
     ft_init_textures(data);
     ft_init_colors(data);
+    ft_init_player(data);
 }
-
-/*void    ft_init_colors(t_data *data)
-{
-    data->ceiling_clr.checked = 0;
-    data->ceiling_clr.r = 0;
-    data->ceiling_clr.g = 0;
-    data->ceiling_clr.b = 0;
-    data->floor_clr.r = 0;
-    data->floor_clr.g = 0;
-    data->floor_clr.b = 0;
-    data->floor_clr.checked = 0;
-}
-
-void    ft_init_textures(t_data *data)
-{
-    data->textures.north = NULL;
-    data->textures.south = NULL;
-    data->textures.west = NULL;
-    data->textures.east = NULL;
-}
-
-int    ft_init_struct(t_data *data)
-{
-
-    data->map = NULL;
-    data->map_height= 0;
-    data->map_width = 0;
-    data->nb_player = 0;
-    data->id_filled = 0;
-    data->file_content = NULL;
-    data->player.pos[0] = 0;
-    data->player.ction = 0;
-    ft_init_textures(data);
-    ft_init_colors(data);
-    return(0);
-}*/
